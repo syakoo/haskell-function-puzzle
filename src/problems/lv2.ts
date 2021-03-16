bb4cc48c2bc04a4db511139ab26e5c5417e23306
PotassiumIodide <potassium.iodide28@gmail.com>
2021-03-16 16:46:13 +0900

    update: add a new problem to lv2.ts

--- src/problems/lv2.ts
+++ src/problems/lv2.ts
@@ -9,4 +9,14 @@ export default [
     choices: ['odd', 'even', 'length', 'map'],
     collectAnswer: ['map', 'even', 'length'],
   },
+  {
+    createdAt: '2021/03/16',
+    env: 'GHC 8.6.5',
+    map: {
+      from: '[1,2,3]', to: '[2,4,6]',
+    },
+    sheet: ['f = foldr (■ . ■) []'],
+    choices: ['map', '(:)', '(++)', 'flip (:)', '(*2)'],
+    collectAnswer: ['(:)', '(*2)'],
+  },
 ] as StoredProblem[]
