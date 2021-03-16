3805682cea446cb6d59699ee5a6a95a78a4ef334
PotassiumIodide <potassium.iodide28@gmail.com>
2021-03-16 13:16:13 +0900

    fix: replace (<= 3) with x <= 3

--- src/problems/lv3.ts
+++ src/problems/lv3.ts
@@ -4,9 +4,12 @@ export default [
   {
     createdAt: '2021/03/16',
     env: 'GHC 8.6.5',
-    map: { from: '[1, 2, 3]', to: '[[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]' },
-    sheet: ['f = ■ (\\x -> ■)'],
-    choices: ['filter', 'filterM', '(<= 3)', '[True, False]'],
+    map: {
+      from: '[1, 2, 3]',
+      to: '[[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]',
+    },
+    sheet: ['import Control.Monad', '', 'f = ■ (\\x -> ■)'],
+    choices: ['filter', 'filterM', 'x <= 3', '[True, False]'],
     collectAnswer: ['filterM', '[True, False]'],
   },
 ] as StoredProblem[]
