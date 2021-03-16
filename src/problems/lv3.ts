44429d7334bd0e7bbc440217f2e72edceed42bd1
syakoo <sakodata0318@gmail.com>
2021-03-16 13:05:21 +0900

    problem: update lv3.

--- src/problems/lv3.ts
+++ src/problems/lv3.ts
@@ -4,8 +4,11 @@ export default [
   {
     createdAt: '2021/03/16',
     env: 'GHC 8.6.5',
-    map: { from: '[1, 2, 3]', to: '[[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]' },
-    sheet: ['f = ■ (\\x -> ■)'],
+    map: {
+      from: '[1, 2, 3]',
+      to: '[[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]',
+    },
+    sheet: ['import Control.Monad', '', 'f = ■ (\\x -> ■)'],
     choices: ['filter', 'filterM', '(<= 3)', '[True, False]'],
     collectAnswer: ['filterM', '[True, False]'],
   },
