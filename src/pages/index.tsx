2cb955b48f68aab4fbdcbdbc4af1f48a8572413e
syakoo <nichijou4009@gmail.com>
2022-05-07 19:43:17 +0900

    fix: rename and move Mata component .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { NextPage } from 'next'
 
 import { Home } from '@/components/pages/Home'
-import { Metas } from '@/components/ui/Metas'
+import { Meta } from '@/components/common/Meta'
 
 // __________
 //
 const TopPage: NextPage = () => {
   return (
     <>
-      <Metas />
+      <Meta />
       <Home />
     </>
   )
